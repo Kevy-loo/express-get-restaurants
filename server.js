5553bb0127291059bbeb58d6ded9465eb0dd9da9
kevin-liu <liu.kevin1997@gmail.com>
2023-03-07 12:27:46 -0500

    finished

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
 
 const port = 3000;
+app.use(express.json());
+
 
 //TODO: Create your GET Request Route Below: 
 
@@ -16,6 +18,26 @@ app.get("/restaurants/:id",async (req, res) => {
     const id = req.params.id
     const data = await Restaurant.findByPk(id)
     res.json(data)
+})
+
+app.delete('/restaurants/:id', async(req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id)
+
+    await restaurant.destroy()
+})
+
+app.post('/restaurants', async (req, res) => {
+    const newRestaurant = Restaurant.build({
+        name: req.body.name,
+        location: req.body.location,
+        cuisine: req.body.cuisine
+    })
+    await newRestaurant.save()
+})
+
+app.put('/restaurants/:id', async (req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    await restaurant.update(req.body)
 })
 
 app.listen(port, () => {
